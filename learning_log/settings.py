82015317844108b40059d9f48a78d141257014e6
Michael Broadbent~ <michael.broadbent95@gmail.com>
2025-03-05 15:00:06 +0000

    Ready to simulate deployment to Heroku

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -34,6 +34,10 @@ INSTALLED_APPS = [
     # My apps
     "learning_logs",
     "users",
+
+    # Third party apps.
+    "bootstrap4",
+
     # Default django apps.
     "django.contrib.admin",
     "django.contrib.auth",
@@ -125,3 +129,10 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# My settings
+LOGIN_URL = 'users:login'
+
+# Heroku settings.
+import django_heroku
+django_heroku.settings(locals())
